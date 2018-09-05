be0621e701d6378409045753692710f2ed09315a
Juan Hernandez <j.hernandezm1984@gmail.com>
2018-09-05 20:31:05 -0300

    eight commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   get 'todos',to: "todos#index"
   get 'todos/new'
   post 'todos', to: "todos#create"
+  get 'todos/list'
   get 'todos/:id', to: "todos#show", as:"todo"
   get 'todos/:id/edit', to: "todos#edit", as: "edit"
   patch 'todos/:id', to: "todos#update", as: "update"
   delete 'todos/:id', to: "todos#destroy", as: "destroy"
+  get 'todos/:id/complete', to: "todos#complete", as: "complete"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
